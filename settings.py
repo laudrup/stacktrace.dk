635a3d80e8ffdcdbf7a55ca106834db5d47b8898
Kasper Laudrup <kl@viewworld.dk>
2011-04-28 22:22:32 +0200

    I probably want to use a real database at some point

--- settings.py
+++ settings.py
@@ -6,21 +6,9 @@ ADMINS = (
     ('Kasper Laudrup', 'laudrup@stacktrace.dk'),
 )
 
-
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': join(PROJECT_PATH, 'db/stacktrace.db'),      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
