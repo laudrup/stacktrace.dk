e2d381a4a44c91e4b82cb9c330c08278fc8f7209
Kasper Laudrup <kl@viewworld.dk>
2011-11-01 10:21:11 +0100

    Create a proper gravatar link

--- homepage/context_processors.py
+++ homepage/context_processors.py
@@ -1,7 +1,11 @@
+import urllib, hashlib
+from django.conf import settings
 from homepage.models import Post
 from homepage.models import Project
 
 def base(request):
+    gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(settings.GRAVATAR_MAIL.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode({'s':str(48)})
     posts = Post.objects.order_by('-pub_date')
     projects = Project.objects.all()
-    return {'posts': posts, 'projects': projects}
+    return {'posts': posts, 'projects': projects, 'gravatar_url': gravatar_url}
