079b9966114f3861dec4bb67b1f77736d6d8376b
Kasper Laudrup <kl@viewworld.dk>
2011-11-02 21:35:25 +0100

    Don't commit commented out code

--- homepage/admin.py
+++ homepage/admin.py
@@ -5,7 +5,6 @@ class GalleryAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_added')
     list_filter = ['date_added']
     date_hierarchy = 'date_added'
-    #prepopulated_fields = {'title_slug': ('title',)}
     filter_horizontal = ('photos',)
 
 admin.site.register(Post)
