41abe9f7e9b1a4bd8f2231f4972e7f039a94582d
Kasper Laudrup <kl@viewworld.dk>
2011-11-22 20:16:01 +0100

    Test that code works before commiting :-)

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ if settings.DEBUG:
     urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
 else:
     urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'homepage.views.media'),
