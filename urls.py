e4425015a3a22b9a787da0c56e2b21cecfabb207
Kasper Laudrup <kl@viewworld.dk>
2011-11-23 19:18:14 +0100

    Give the user the possibility to logout when logged in

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
   (r'^admin/', include(admin.site.urls)),
   (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+  (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
   (r'^/?', include('homepage.urls'))
 )
 
