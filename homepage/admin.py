e95170c88d3b1330f3da445dd4e937001b3bf74d
Kasper Laudrup <kl@viewworld.dk>
2011-11-24 20:43:32 +0100

    Add photos belonging to a gallery to the admin interface

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from models import *
 
+class PhotoInline(admin.StackedInline):
+    model = Photo
+
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+
 admin.site.register(Post)
 admin.site.register(Project)
 admin.site.register(Comment)
 admin.site.register(GalleryUpload)
-admin.site.register(Gallery)
+admin.site.register(Gallery, GalleryAdmin)
