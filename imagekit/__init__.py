3366b009b8cdb526494b07cc5a78d6e429b8bf56
Kasper Laudrup <kl@viewworld.dk>
2011-11-25 18:08:02 +0100

    Remove ImageKit library

--- imagekit/__init__.py
+++
@@ -1,4 +0,0 @@
-__title__ = 'django-imagekit'
-__author__ = 'Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett, Matthew Tretter, Eric Eldredge'
-__version__ = (1, 1, 0, 'dev', 0)
-__license__ = 'BSD'
