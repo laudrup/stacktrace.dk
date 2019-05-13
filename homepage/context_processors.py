a80684d9e1be0b4625849a983d9506e899a1000e
Kasper Laudrup <laudrup@stacktrace.dk>
2019-05-13 19:28:59 +0200

    The urllib.urlencode function has been moved in python 3

--- homepage/context_processors.py
+++ homepage/context_processors.py
@@ -1,12 +1,13 @@
-import urllib, hashlib
+import urllib.parse
+import hashlib
 from django.conf import settings
 from homepage.models import Post
 from homepage.models import Project
 from django.conf import settings
 
 def base(request):
-    gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(settings.GRAVATAR_MAIL.lower()).hexdigest() + "?"
-    gravatar_url += urllib.urlencode({'s':str(48)})
+    gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(settings.GRAVATAR_MAIL.lower().encode('utf-8')).hexdigest() + "?"
+    gravatar_url += urllib.parse.urlencode({'s':str(48)})
     posts = Post.objects.order_by('-pub_date')
     projects = Project.objects.all()
     return {'posts': posts, 'projects': projects,  'gravatar_url': gravatar_url, 'STATIC_URL': settings.STATIC_URL}
