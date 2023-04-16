0e29bad0f4451ecd62d1ca0bad76a34856ba35da
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-16 14:42:04 +0200

    Upgrade to Django 4

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import include, re_path, path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -8,15 +8,15 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = [
-  url(r'^admin/?', admin.site.urls),
-  url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-  url(r'^logout/?$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-  url(r'^', include('homepage.urls'))
+    path('', include('homepage.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', homepage_views.media),
-]
+        re_path('media/(?P<path>.*)$', homepage_views.media)
+    ]
